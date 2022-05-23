046245532c490120da51c66b5897f6d5467dbfe1
Arhan99 <Arhan99>
2022-05-23 13:36:02 +0200

    tz

--- my-app/src/components/Contacts/Contacts.jsx
+++ my-app/src/components/Contacts/Contacts.jsx
@@ -1,118 +1,138 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import s from './Contacts.module.css';
+import s from "./Contacts.module.css";
 import { useEffect } from "react";
 import ChangeModal from "../../Modal/ChangeModal";
 import { useState } from "react";
+import { deleteContact } from "../../redux/reducers/auth";
 import ChangeContact from "../ModalComponents/ChangeContact";
+import { useDispatch } from "react-redux";
 
-function Contacts ({isAuth, isChanged, contacts, user, getContactsThunk, deleteContact, updateContactsThunk, changeContact, setFlagData }) {
+function Contacts({
+  isAuth,
+  isChanged,
+  contacts,
+  user,
+  getContactsThunk,
+  //   deleteContact,
+  updateContactsThunk,
+  changeContact,
+  setFlagData,
+}) {
+  console.log("CONTACTS", contacts);
+  const [contact, setContact] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
+  const [count, setCount] = useState(0);
+  const dispatch = useDispatch();
 
-    const [contact, setContact] = useState({})
-    const [isOpen, setIsOpen] = useState(false)
-    const [count, setCount] = useState(0)
+  // const i = 0;
 
-    // const i = 0;
+  useEffect(() => {
+    if (isAuth) return getContactsThunk(user.id);
+  }, [isChanged]);
 
+  // useEffect(()=>{
+  //     updateContactsThunk(user)
+  // },[user])
 
-    useEffect(()=>{
-        if (isAuth) return getContactsThunk(user.id);
-    },[isChanged])
+  useEffect(() => {
+    updateContactsThunk(user);
+  }, [count]);
 
-    // useEffect(()=>{
-    //     updateContactsThunk(user)
-    // },[user])
+  let openModal = (contact) => {
+    setContact(contact);
+    setIsOpen(!isOpen);
+    console.log(contact);
+  };
 
-    useEffect(()=>{
-        updateContactsThunk(user)
-    },[count])
+  const closeModal = () => {
+    setIsOpen(!isOpen);
+  };
 
-    let openModal = (contact) =>{
-        setContact(contact)
-        setIsOpen(!isOpen)
-        console.log(contact);
-    }
+  if (!isAuth) return <Navigate to={"/"} />;
 
-    const closeModal = () =>{
-        setIsOpen(!isOpen)
-    }
+  const deleteContactButton = async (id, user) => {
+    dispatch(deleteContact(id));
+    setCount((prev) => ++prev);
+    console.log(count);
+    // updateContactsThunk(user);
+    // setFlagData(!isChanged)
+  };
 
-    if (!isAuth) return <Navigate to={"/"} />;
+  const changeContactButton = (contact) => {
+    // contacts.map((e)=> e = e.id==contact.id?contact:e)
+    changeContact(contacts);
+    setCount((prev) => ++prev);
+  };
 
-    const deleteContactButton = async (id, user) => {
-        await deleteContact(id);
-        setCount((prev) => ++prev)
-        console.log(count);
-        // updateContactsThunk(user);
-        // setFlagData(!isChanged)
+  // var promise = new Promise(function(resolve, reject) {
+  //     resolve(changeContactButton)
+  // })
+  // promise.then(function(){
+  //     updateContactsThunk(user);
+  //     console.log('change work');
+  // })
 
-    }
+  // async function changeAsync(contact) {
+  //     try{
+  //         console.log('change async');
+  //         await changeContactButton(contact);
+  //         console.log('change async 2');
+  //         await updateContactsThunk(user);
+  //         console.log('change async 3');
 
-    const changeContactButton = (contact) => {
-        // contacts.map((e)=> e = e.id==contact.id?contact:e) 
-        changeContact(contacts);
-        setCount((prev) => ++prev)
+  //     }
+  //     catch{}
+  // }
 
-    }
-
-    // var promise = new Promise(function(resolve, reject) {
-    //     resolve(changeContactButton)
-    // })
-    // promise.then(function(){
-    //     updateContactsThunk(user);
-    //     console.log('change work');
-    // })
-
-    // async function changeAsync(contact) {
-    //     try{
-    //         console.log('change async');
-    //         await changeContactButton(contact);
-    //         console.log('change async 2');
-    //         await updateContactsThunk(user);
-    //         console.log('change async 3');
-
-    //     }
-    //     catch{}
-    // }
-
-
-    const contactsElements = contacts.map( (c, id) =>
-        <div key={c.id} className={s.contact}>
+  const contactsElements = contacts.map((c, id) => (
+    <div key={c.id} className={s.contact}>
+      <div>
+        <img src={c.photo} alt="ava" className={s.img} />
+      </div>
+      <div className={s.content}>
         <div>
-            <img src={c.photo} alt='ava' className={s.img}/>
+          <h1>Name: {c.name}</h1>
         </div>
-        <div className={s.content}>
-            <div>
-                <h1 >Name: {c.name}</h1>
-            </div>
-            <div>
-                <h2>Lastname: {c.lastname}</h2>
-            </div>
-            <div>
-                <p>Age: {c.age}</p>
-            </div>
-            <div>
-                <p>Phone: {c.number}</p>
-            </div>
+        <div>
+          <h2>Lastname: {c.lastname}</h2>
         </div>
-        <div className={s.buttons}>
-            <div className={s.buttonWrap}>
-                <button onClick={()=> openModal(c)} className={s.button}>Edit</button>
-            </div>
-            <div className={s.buttonWrap}>
-                <button onClick={() => deleteContactButton(id, user)} className={s.button}>Delete</button>
-            </div>
+        <div>
+          <p>Age: {c.age}</p>
         </div>
+        <div>
+          <p>Phone: {c.number}</p>
+        </div>
+      </div>
+      <div className={s.buttons}>
+        <div className={s.buttonWrap}>
+          <button onClick={() => openModal(c)} className={s.button}>
+            Edit
+          </button>
+        </div>
+        <div className={s.buttonWrap}>
+          <button
+            onClick={() => deleteContactButton(id, user)}
+            className={s.button}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
     </div>
-    
-   )
+  ));
 
-    return(
-        <div className={s.contacts} >
-            {contactsElements}
-            <ChangeModal title='Change contact' isOpened={isOpen} onModalClose={()=> closeModal()}
-             contact={contact} onSubmit={(cnt)=>changeContactButton(cnt)}/>
-        </div>
-    )
+  return (
+    <div className={s.contacts}>
+      {contactsElements}
+      <ChangeModal
+        title="Change contact"
+        isOpened={isOpen}
+        onModalClose={() => closeModal()}
+        contact={contact}
+        onSubmit={(cnt) => changeContactButton(cnt)}
+      />
+    </div>
+  );
 }
 export default Contacts;
--- my-app/src/redux/reducers/auth.js
+++ my-app/src/redux/reducers/auth.js
@@ -1,118 +1,117 @@
 import { userAPI } from "../../api/api";
 import { contactsAPI } from "../../api/api";
 
-const SET_USER_DATA = 'SET_USER_DATA';
-const LOGOUT = 'LOGOUT';
+const SET_USER_DATA = "SET_USER_DATA";
+const LOGOUT = "LOGOUT";
 
-const ADD_CONTACT = 'ADD_CONTACT';
-const CHANGE_CONTACT = 'CHANGE_CONTACT';
-const DELETE_CONTACT = 'DELETE_CONTACT';
+const ADD_CONTACT = "ADD_CONTACT";
+const CHANGE_CONTACT = "CHANGE_CONTACT";
+const DELETE_CONTACT = "DELETE_CONTACT";
 
 export const setUserData = (user) => ({
-    type: 'SET_USER_DATA',
-    data: user
-})
+  type: "SET_USER_DATA",
+  data: user,
+});
 export const logout = () => {
-    return {type: 'LOGOUT'}
-}
-
-
+  return { type: "LOGOUT" };
+};
 
 export const addContact = (message) => ({
-    type: 'ADD_CONTACT',
-    contact: message
+  type: "ADD_CONTACT",
+  contact: message,
 });
 
 export const changeContact = (message) => ({
-    type: 'CHANGE_CONTACT',
-    contacts: message
-
+  type: "CHANGE_CONTACT",
+  contacts: message,
 });
 
 export const deleteContact = (message) => ({
-    type: 'DELETE_CONTACT',
-    index: message
-
+  type: "DELETE_CONTACT",
+  index: message,
 });
 
-
-const initialState =  {
-    user:{
-        userId: null,
-        login: null,
-        password: null,
-        contacts: null,
-    },
-    isAuth: false
+const initialState = {
+  user: {
+    userId: null,
+    login: null,
+    password: null,
+    contacts: null,
+  },
+  isAuth: false,
 };
 
 export const auth = (state = initialState, action) => {
-    switch (action.type){
-            case SET_USER_DATA:
-            return {
-                ...state,
-                user: action.data,
-                isAuth: true
-
-            };
-            case LOGOUT:
-                return{
-                    ...state,
-                    user: null,
-                    isAuth: false,
-                };
-            case ADD_CONTACT:
-                return{
-                    ...state,
-                    user: {
-                        ...state.user, contacts: [...state.user.contacts , action.contact]
-                    }
-                };
-            case CHANGE_CONTACT:
-                if(!!action.contacts)
-                return{
-                    ...state,
-                    user: {
-                        ...state.user, 
-                        contacts: [...state.user.contacts.map(c => {
-                            for(let i =0; i<action.contacts.length; i++ ){
-                                if(action.contacts[i].id===c.id) return action.contacts[i]
-                            }
-                            return c
-                        })],
-                    },
-                   
-                };
-                return{
-                    ...state,
+  switch (action.type) {
+    case SET_USER_DATA:
+      return {
+        ...state,
+        user: action.data,
+        isAuth: true,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        user: null,
+        isAuth: false,
+      };
+    case ADD_CONTACT:
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          contacts: [...state.user.contacts, action.contact],
+        },
+      };
+    case CHANGE_CONTACT:
+      if (!!action.contacts)
+        return {
+          ...state,
+          user: {
+            ...state.user,
+            contacts: [
+              ...state.user.contacts.map((c) => {
+                for (let i = 0; i < action.contacts.length; i++) {
+                  if (action.contacts[i].id === c.id) return action.contacts[i];
                 }
-            case DELETE_CONTACT:
-                return{
-                    ...state,
-                    user: {
-                        ...state.user,
-                        contacts: [...state.user.contacts.slice(0, action.index).concat(...state.user.contacts.slice(action.index + 1))]
-                    }
-                }
-        default:
-            return {
-                ...state
-            };
-    }
-}
+                return c;
+              }),
+            ],
+          },
+        };
+      return {
+        ...state,
+      };
+    case DELETE_CONTACT:
+      console.log(action.index);
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          contacts: [
+            ...state.user.contacts.slice(0, action.index),
+            ...state.user.contacts.slice(action.index + 1),
+          ],
+          // contacts: [...state.user.contacts.slice(0, action.index).concat(...state.user.contacts.slice(action.index + 1))]
+        },
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
 
-export const getUserThunk = (login, password ) => {
-    return( dispath ) => {
-        userAPI.getUser(login, password ).then( response => {
-            dispath( setUserData( response[0] ) )
-        } )
-    }
-}
+export const getUserThunk = (login, password) => {
+  return (dispath) => {
+    userAPI.getUser(login, password).then((response) => {
+      dispath(setUserData(response[0]));
+    });
+  };
+};
 
 export const updateContactsThunk = (user) => {
-    return(dispath) =>{
-        contactsAPI.updateContacts(user)
-
-    }
-}
-
+  return (dispath) => {
+    contactsAPI.updateContacts(user);
+  };
+};
